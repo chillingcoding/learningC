77d2037b1730716ef6f525fcc2ea666380f53cb8
Ho Le Minh Hai <haihlm.code@gmail.com>
2024-01-24 20:20:02 +0700

    Update 2 lesson of C

---
+++ 2_VariableInAndOutOfMain/2_VariableInAndOutOfMain.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int a;
+int main()
+{
+    int b ;
+    // The value init out of main function will be 0
+    printf("Number integer out main = %d\n",a);
+    // The value init in main function will be a random number
+    printf("Number integer in main = %d", b);
+
+    // ?? So that why ??
+    
+    return 0;
+}
